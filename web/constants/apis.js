fe90edfdd4bb4420526b5880c3d9babefe793be6
Doug Colaizzo <dcolaizzo@gmail.com>
2020-04-26 13:28:43 -0700

    Update prod URL

--- web/constants/apis.js
+++ web/constants/apis.js
@@ -1,2 +1,2 @@
 window.API_URL_DEV = "https://jvcl2b12pc.execute-api.us-east-1.amazonaws.com/dev/api"
-window.API_URL_PROD = "https://84nkobp344.execute-api.us-east-1.amazonaws.com/prod/api"
+window.API_URL_PROD = "https://vzj7exovg6.execute-api.us-east-1.amazonaws.com/prod/api"
